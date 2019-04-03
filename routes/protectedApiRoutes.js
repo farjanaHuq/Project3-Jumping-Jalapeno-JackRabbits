d84d135c8058fc6bf14cd608fe97309f9c84b3bb
Richard Fitkin <rfitkin90@gmail.com>
2019-04-03 11:11:32 -0700

    authentication working

--- routes/protectedApiRoutes.js
+++ routes/protectedApiRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-// get all books
 router.get('/check', (req, res) => {
    res.json('protected route');
 });

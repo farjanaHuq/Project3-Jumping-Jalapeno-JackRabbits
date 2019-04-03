054c60a9c23d34c0a6c7dde3ef56e85409ab264c
Farjana Huq <fa.huq03@gmail.com>
2019-04-02 21:28:32 -0700

    confirm password input field added to the modal

--- client/src/components/SignupModal.js
+++ client/src/components/SignupModal.js
@@ -22,7 +22,8 @@ class SignupModal extends Component {
          password: document.getElementById('passwordField').value
       }
       console.log('signupData:', signupData);
-
+      
+      
       // post it to api
       axios.post('/api/auth/register', signupData)
          .then(res => {
@@ -52,6 +53,11 @@ class SignupModal extends Component {
                      <Form.Group>
                         <Form.Label>Password</Form.Label>
                         <Form.Control id="passwordField" type="password" placeholder="Password" />
+                     </Form.Group>
+                     <Form.Group>
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control id="confirmPasswordField" type="confirmPassword" placeholder="confirmPassword" />
+                        <Form.Text className="text-muted error-message" id="email-error-message"></Form.Text>
                      </Form.Group>
                   </Modal.Body>
                   <Modal.Footer>

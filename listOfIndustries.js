bdeca5b3d786f48a5dfade58fbaf4f4cb0fdfde9
Farjana Huq <fa.huq03@gmail.com>
2019-04-09 23:24:40 -0700

    listOfIndustries.js

---
+++ listOfIndustries.js
@@ -0,0 +1,37 @@
+const listOfIndustries = [
+
+    "Government Operations and Politics",
+    "Environmental Protection",
+    "Armed Forces and National Security",
+    "Public Lands and Natural Resources",
+    "Economics and Public Finance",
+    "Public Lands and Natural Resources",
+    "International Affairs",
+    "Public Lands and Natural Resources",
+    "Water Resources Development",
+    "Finance and Financial Sector",
+    "Government Operations and Politics",
+    "International Affairs", 
+    "Congress",
+    "Emergency Management",
+    "International Affairs",
+    "Health",
+    "Immigration",
+    "Energy",
+    "Science, Technology, Communications",
+    "Arts, Culture, Religion",
+    "Science, Technology, Communications",
+    "Agriculture and Food",  
+    "Commerce",
+    "Law",
+    "Animals",
+    "Native Americans",
+    "Transportation and Public Works",
+    "Sports and Recreation",
+    "Taxation",
+    "Social welfare",
+    "Crime and law enforcement"
+
+]
+
+module.exports = listOfIndustries;

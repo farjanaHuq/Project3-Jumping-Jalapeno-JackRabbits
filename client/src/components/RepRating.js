f714390541bec53415cea7c74e51b88591e05540
rfitkin90 <rfitkin90@gmail.com>
2019-04-12 23:17:15 -0700

    rep cid and name becoming undefined in props

--- client/src/components/RepRating.js
+++ client/src/components/RepRating.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../style.css';
-import axios from "axios";
 
 class RepRating extends Component {
    constructor(props) {
@@ -14,51 +13,16 @@ class RepRating extends Component {
 
    }
 
-   // calculateRatingPercent = () => {
-   //    if (this.props.repRatingAndComments) {
-   //       const upVotes = this.props.repRatingAndComments.upVotes.length;
-   //       const downVotes = this.props.repRatingAndComments.downVotes.length;
-   //       const totalVotes = upVotes + downVotes;
-   //       const approvalRating = Math.round(((upVotes / totalVotes)) * 100);
-   //       if (approvalRating) {
-   //          return approvalRating + '%';
-   //       } else {
-   //          return '0%';
-   //       }
-   //    }
-   // }
-
-   upVoteRep = () => {
-      this.rateRep('upvote');
-   }
-
-   downVoteRep = () => {
-      this.rateRep('downvote');
-   }
-
-   rateRep = voteType => {
-      const repCid = this.props.repRatingAndComments.repCid;
-      const userID = this.props.userData.userID;
-      axios.put(`/api/secureCommentAndRatingRoutes/raterepresentative/${repCid}/${voteType}/${userID}`)
-         .then(resp => console.log('up vote resp:', resp))
-         .catch(err => (console.log('up vote rep err:', err)));
-   }
-
-   logProps = () => {
-      if (this.props.userData && this.props.repRatingAndComments) {
-         console.log('rep rating props:', this.props);
-      }
-   }
-
    renderRepRatingDiv = () => {
-      if (this.props.repRatingAndComments && this.props.userData.userID) {
+      if (this.props.repRatingAndComments && this.props.userData) {
          return (
             <span id="rep-rating">
+            {console.log(this.props.repRatingAndComments.upVotes)}
                <FontAwesomeIcon icon="thumbs-up" id="upvote-rep" onClick={this.props.upVoteRep}
                   style={{ color: this.props.repRatingAndComments.upVotes.includes(this.props.userData.userID) ? 'rgb(0, 107, 207)' : 'white' }}
                />
                &nbsp;&nbsp;{this.props.calculateRatingPercent()}&nbsp;&nbsp;
-            <FontAwesomeIcon icon="thumbs-down" id="downvote-rep" onClick={this.props.downVoteRep}
+               <FontAwesomeIcon icon="thumbs-down" id="downvote-rep" onClick={this.props.downVoteRep}
                   style={{ color: this.props.repRatingAndComments.downVotes.includes(this.props.userData.userID) ? 'rgb(0, 107, 207)' : 'white' }}
                />
             </span>

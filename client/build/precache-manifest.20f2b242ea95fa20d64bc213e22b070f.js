e7e05f2ba336129f4fd7bf9d41682d980fdf026c
Richard Fitkin <rfitkin90@gmail.com>
2019-04-15 20:52:35 -0700

    fixed react-scripts

---
+++ client/build/precache-manifest.20f2b242ea95fa20d64bc213e22b070f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66731761a725642599ea",
+    "url": "/static/js/main.c0da3b51.chunk.js"
+  },
+  {
+    "revision": "12709e433341745c8e61",
+    "url": "/static/js/2.5dadd519.chunk.js"
+  },
+  {
+    "revision": "66731761a725642599ea",
+    "url": "/static/css/main.73067edd.chunk.css"
+  },
+  {
+    "revision": "12709e433341745c8e61",
+    "url": "/static/css/2.4422483a.chunk.css"
+  },
+  {
+    "revision": "a349766bd5280c529041989a4fc28790",
+    "url": "/index.html"
+  }
+];
